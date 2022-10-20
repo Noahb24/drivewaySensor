cca3facaef9a581d114fd1c2b775582c9c60d800
Noah Blochowitz <blochowitz.noah@gmail.com>
2022-10-20 01:19:27 +0000

    add request to pi file

--- drivewaySensor.py
+++ drivewaySensor.py
@@ -1,13 +1,16 @@
 from gpiozero import LED, Button
 from signal import pause
+import requests
 
 print ('Driveway Sensor is starting')
 
 button = Button(18)
 
-def onButtonPressed():
+def triggerSensor():
+    x = requests.post('http://192.168.1.138:8080/driveway')
+    print(x.text)
     print('Drive tripped')
 
-button.when_pressed = onButtonPressed
+button.when_pressed = triggerSensor
 
 pause()

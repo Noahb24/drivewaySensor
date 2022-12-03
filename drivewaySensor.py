911b0600df3d60cfc2fb3e39c033c480d62b34c0
Noah Pi <Blochowitz.noah@gmail.com>
2022-12-03 16:14:11 -0700

    pi commit

--- drivewaySensor.py
+++ drivewaySensor.py
@@ -1,16 +1,25 @@
 from gpiozero import LED, Button
 from signal import pause
 import requests
+import time
 
 print ('Driveway Sensor is starting')
 
-button = Button(18)
+button = Button(4, pull_up=False)
 
-def triggerSensor():
-    x = requests.post('http://192.168.0.178:3500/driveway/tripped')
-    print(x.text)
-    print('Drive tripped')
+triggered = False
 
-button.when_pressed = triggerSensor
+def triggerSensor(state):
+    if not state:
+        x = requests.post('http://192.168.0.178:3500/driveway/tripped')
+        print('Drive tripped')
 
-pause()
+while True:
+    if button.is_pressed:
+	print('closed')
+	triggerSensor(triggered)
+	triggered = True
+    else:
+	triggered = False
+    time.sleep(0.5)
+
